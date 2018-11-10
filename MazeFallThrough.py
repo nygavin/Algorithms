b3bd704d46386ac13f424066594b4db89ce417fd
nygavin <44912520+nygavin@users.noreply.github.com>
2018-11-10 14:38:57 -0500

    Update MazeFallThrough.py

--- MazeFallThrough.py
+++ MazeFallThrough.py
@@ -1,6 +1,6 @@
 #objective is to find quickest path down
 class Solution(object):
-	maze = [ [0,0,1,0,0],
+	maze = [ [0,1,1,0,0],
 	[1,1,0,1,1],
 	[0,1,0,0,1],
 	[0,1,1,1,1],
@@ -12,26 +12,26 @@ class Solution(object):
 	def traverse(self, row, col, direction=1):#1 for right -1 for left
 		pathlen = 1;
 		while(col < self.COLS and col > 0):
-			pathlen +=1
+			
 			if self.maze[row+1][col] > 0:
 				return pathlen + self.maze[row+1][col]
 			col +=direction
+			pathlen +=1
+			
 		#return max lenght which would never be considered in minum
 		return self.ROWS*self.COLS
 		
 		
 	def calcRow(self, row):
 		for col in range(self.COLS):
-			if row > 0 and self.maze[row-1][col] > 0:
-				self.maze[row][col] = min(self.traverse(row, col, -1),self.traverse(row, col, 1))
-
-		#for col in range(cols):#only traverse cells which have immediate connection to above row
-		#if maze[row, col-1] > 0:
-    	#		print maze#maze[row,col] = min(traverse( row, col, 1), self.traverse(row, col, -1))
-
+			if row > 0 and self.maze[row][col] > 0 and  self.maze[row-1][col] > 0\
+				or row ==0 and  self.maze[row][col] > 0:
+				self.maze[row][col] = min(self.traverse(row, col, -1), self.traverse(row, col, 1))
+		#once traversed we clean
+		self.maze[row] = [val if val>1 else 0 for val in self.maze[row]  ]
+		
 a= Solution()
 for row in reversed(range(a.ROWS-1)):
 	a.calcRow(row)
 print a.maze
-	
-#print maze# your code goes here
+print "Min Path Is: " + str(min([val for val in a.maze[0] if val>0 ]))
