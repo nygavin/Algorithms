cf5b8faf3d5fc390050c43f926d480e3ca2b4b81
nygavin <nygavin@gmail.com>
2018-11-10 16:39:13 -0500

    fixed boundary issue and added iteration count

--- MazeFallThrough.py
+++ MazeFallThrough.py
@@ -1,21 +1,23 @@
 #objective is to find quickest path down
 class Solution(object):
-	maze = [ [0,1,1,0,0],
-	[1,1,0,1,1],
-	[0,1,0,0,1],
-	[0,1,1,1,1],
-	[1,0,1,0,1]]
+	count = 0
+	maze = [[0, 1, 1, 0, 0],
+			[1, 1, 0, 1, 1],
+			[0, 1, 0, 0, 1],
+			[0, 1, 1, 1, 1],
+			[1, 0, 1, 0, 1]]
 	
 	ROWS = len(maze)
 	COLS = len(maze[0])
 	
 	def traverse(self, row, col, direction=1):#1 for right -1 for left
 		pathlen = 1;
-		while(col < self.COLS and col > 0):
-			
+		while(col < self.COLS and col >= 0):
+
 			if self.maze[row+1][col] > 0:
 				return pathlen + self.maze[row+1][col]
 			col +=direction
+			self.count += 1
 			pathlen +=1
 			
 		#return max lenght which would never be considered in minum
@@ -24,6 +26,7 @@ class Solution(object):
 		
 	def calcRow(self, row):
 		for col in range(self.COLS):
+			self.count +=1
 			if row > 0 and self.maze[row][col] > 0 and  self.maze[row-1][col] > 0\
 				or row ==0 and  self.maze[row][col] > 0:
 				self.maze[row][col] = min(self.traverse(row, col, -1), self.traverse(row, col, 1))
@@ -34,4 +37,5 @@ a= Solution()
 for row in reversed(range(a.ROWS-1)):
 	a.calcRow(row)
 print a.maze
+print "Iterations: " + str(a.count)
 print "Min Path Is: " + str(min([val for val in a.maze[0] if val>0 ]))
